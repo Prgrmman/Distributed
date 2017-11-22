0fca4312efd047d2e3fe5681e4d86276584017e2
Jonathan Terner <jterner1@binghamton.edu>
2017-11-22 12:09:54 -0500

    proj4: laid some of the ground work
    Set up the project, got some design docs going,
    got the thrift file done.

---
+++ proj4/store.thrift
@@ -0,0 +1,33 @@
+
+exception SystemException {
+  1: optional string message
+}
+
+
+struct Value {
+  1: i32 value;
+  2: string content;
+  3: double timestamp; // remember, the coordinator sets this
+}
+
+
+service KeyValueStore {
+  
+  // RPC used by clients
+  Value get_key(1: i32 level)
+    throws (1: SystemException systemException),
+
+  
+  // RPC used by clients
+  void put_key_value(1: Value value, 2: i32 level)
+    throws (1: SystemException systemException),
+
+
+
+  // RPC used between replicas.
+  // tells a replica that it is alive again
+  void heartbeat(1: string replica_name),
+}
+
+
+
